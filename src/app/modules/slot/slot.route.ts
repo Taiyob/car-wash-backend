6ae869fecfc3e81be6f0afd499bc9146c8c28d1e
Taiyob <oli1412001@gmail.com>
2024-09-23 18:31:59 +0600

    commit

---
+++ src/app/modules/slot/slot.route.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import zodValidationMiddleware from '../../middleware/zodValidationMiddleware';
+import { SlotValidations } from './slot.validation';
+import { SlotControllers } from './slot.controller';
+
+const router = express.Router();
+
+router.post(
+  '/',
+  zodValidationMiddleware(SlotValidations.createSlotValidationSchema),
+  SlotControllers.createSlot,
+);
+
+router.get('/availability', SlotControllers.getAllAvailableSlot);
+
+router.get('/:serviceId', SlotControllers.getSlotsByServiceId);
+
+export const SlotRoutes = router;
